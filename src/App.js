e8a81b13c1e949a0b48448022e83198a81943168
Chris Marshall <chris@codegoalie.com>
2020-05-13 13:46:42 -0400

    Cycle colors and animate letters

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     <div className="App">
       <ThemeProvider theme={theme}>
         <Header>
-          <SplitText copy="WASH YOUR HANDS!" />
+          <SplitText copy="Wash Your Hands!" />
         </Header>
       </ThemeProvider>
     </div>
@@ -18,12 +18,62 @@ function App() {
 export default App;
 
 const Header = styled.div`
+  @keyframes scatter {
+    0% {
+      top: 0;
+    }
+    50% {
+      top: -10px;
+    }
+    100% {
+      top: 0px;
+    }
+  }
+
+  font-size: 10rem;
+  text-transform: uppercase;
+  font-family: "Archivo Black", "Archivo", sans-serif;
+  font-weight: normal;
+  display: block;
+  width: 666px;
+  max-width: 80vw;
+  min-height: 90px;
+  height: auto;
+  text-align: center;
+  margin: 10rem auto;
+  margin: auto;
+  transform(translateY(50vh));
+
   color: ${props => props.theme.main};
-  text-shadow: ${props => textShadow(0.25, 8, props.theme.shadow)};
+  text-shadow: ${props => textShadow(0.25, 6, props.theme.shadow)};
+
+  & span {
+    position: relative;
+    animation: scatter 1.75s infinite;
+  }
 
   & span:nth-child(2n) {
     color: ${props => props.theme.secondary};
-    text-shadow: ${props => textShadow(0.25, 8, props.theme.secondaryShadow)};
+    text-shadow: ${props => textShadow(0.25, 6, props.theme.secondaryShadow)};
+    animation-delay: 0.3s;
+  }
+
+  & span:nth-child(3n) {
+    color: #ffd913;
+    text-shadow: ${props => textShadow(0.25, 6, "#6EC0A9")};
+    animation-delay: 0.15s;
+  }
+
+  & span:nth-child(5n) {
+    color: #555bff;
+    text-shadow: ${props => textShadow(0.25, 6, "#E485F8")};
+    animation-delay: 0.4s;
+  }
+
+  & span:nth-child(7n), & span:nth-child(11n) {
+    color: #ff9c55;
+    text-shadow: ${props => textShadow(0.25, 7, "#FF5555")};
+    animation-delay: 0.25s;
   }
 `;
 
--- src/index.js
+++ src/index.js
@@ -6,29 +6,39 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
 const GlobalStyles = createGlobalStyle`
-* {
-  box-sizing: border-box;
-  -moz-box-sizing: border-box;
-  -webkit-box-sizing: border-box;
-}
-body {
-  font-size: 8pt;
-  background-color: #282c34;
-  color: white;
-}
-.App {
-  text-align: center;
-  padding: 2rem;
-  max-width: 1280px;
-  margin: auto;
-  font-size: 10rem;
-  font-weight: 900;
-}
-@media (max-width: 768px) {
-  .App {
-    padding: 1rem;
+  * {
+    box-sizing: border-box;
+    -moz-box-sizing: border-box;
+    -webkit-box-sizing: border-box;
   }
-}
+
+  body {
+    font-size: 8pt;
+    background-color: #E4FFF7;
+    color: white;
+  }
+
+  .App {
+    text-align: center;
+    padding: 2rem;
+    max-width: 1280px;
+    margin: auto;
+    font-size: 10rem;
+    font-weight: 900;
+    display: flex;
+    align-items: center;
+    height: 100vh;
+  }
+
+  @media (max-width: 768px) {
+    .App {
+      padding: 1rem;
+    }
+  }
+
+  @media screen and (prefers-reduced-motion: reduce) {
+    animation: none;
+  }
 `;
 
 ReactDOM.render(
